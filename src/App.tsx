6af02183ca567c522c3b31b1fef146e3abcac2e4
nvminh <nvminh@tma.com.vn>
2022-04-25 09:29:58 +0700

    config done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
          <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>Hello new World</p>
+            <p>Hello World</p>
          </header>
       </div>
    );
